a28439858c49f862327e0ee0d26cbbf8386d3b14
Gregory Marshall <gmarshall@dawan.fr>
2023-03-08 10:26:54 +0100

    Project INIT

---
+++ src/test/java/com/projects/garages/GaragesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.garages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
